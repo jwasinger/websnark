4445c1781c3e62b4c5bebc6c935af1639aafad16
Jared Wasinger <j-wasinger@hotmail.com>
2019-10-29 20:14:19 +0900

    more

--- example/bn128/serialize_proof.py
+++ example/bn128/serialize_proof.py
@@ -5,6 +5,9 @@ public_input = None
 vk = None
 proof = None
 
+def count_to_8_bytes(count):
+  return binascii.hexlify(vk['nPublic'].to_bytes(8, 'little')).decode()
+
 def num_to_hex(g1):
   result = bytearray.fromhex(hex(int(g1))[2:].zfill(64))
   result.reverse()
@@ -32,5 +35,24 @@ proof_a = serialize_f1(proof['pi_a'])
 proof_b = serialize_f2(proof['pi_b'])
 proof_c = serialize_f1(proof['pi_c'])
 
+vk_a = serialize_f1(vk['vk_alfa_1'])
+vk_b = serialize_f2(vk['vk_beta_2'])
+vk_g = serialize_f2(vk['vk_gamma_2'])
+vk_d = serialize_f2(vk['vk_delta_2'])
+
+ics = ''
+
+num_ics = count_to_8_bytes(len(vk['IC']))
+ics += num_ics
+
+for ic in vk['IC']:
+  ics += serialize_f1(ic)
+
+num_public = count_to_8_bytes(len(public_input['input']))
+public_inputs = num_public
+
+for p in public_input['input']:
+  public_inputs += num_to_hex(p)
+
 print("serialized proof:")
-print(proof_a)
+print(vk_a + vk_b + vk_g + vk_d + proof_a + proof_b + proof_c + ics + public_inputs)
